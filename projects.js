ff8b5c5a46caa6ba06f10f768d7e406c7c64675a
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-08-12 18:10:15 -0500

    project render functions

--- projects.js
+++ projects.js
@@ -1,8 +1,6 @@
-let projectArr = [{
-    name: "Test Project 1",
-    description: "Test Project Description"
-}
-]
+let projectArr = []
+
+let lastUpdated = new Date()
 
 const renderToDom = (divId, textToRender) => {
     const selectedDiv = document.querySelector(divId)
@@ -17,6 +15,7 @@ const projectResult = (projectArray) => {
         <div class="card-body">
             <h5 class="card-text">${project.name}</h5>
             <p class="card-text">${project.description}</p>
+            <p class="card-text">${"Last Updated:" + " " + lastUpdated}</p>
         </div>
     </div>
     `;
@@ -25,8 +24,49 @@ const projectResult = (projectArray) => {
     renderToDom('#projectDiv', domString);
 };
 
+const handleProjectForm = (event) => {
+    event.preventDefault();
+    const newProject = {
+        name: document.querySelector('#name').value,
+        description: document.querySelector('#description').value,
+        date: document.querySelector('#lastUpdated')
+    };
+
+    projectArr.push(newProject);
+    projectResult(projectArr);
+
+
+    document.querySelector('#projectForm').reset()
+}
+
+
+  const projectForm = () => {
+    const domString = `
+    <form id="projectForm" class="PForm">
+      <div class="Label-Text">
+        <label for="name" class="form-body">Project Name</label>
+        <input required type="text" class="form-control" id="name">
+      </div>
+      <div class="Label-Text">
+        <label "for="description">Description (Optional)</label>
+        <textarea class="form-control" id="description" rows="3"></textarea>
+        <button type="submit" class="btn btn-primary">Create Project</button>
+    </div>
+    </form>
+   `;
+    renderToDom('#formContainer', domString);
+
+    projectFormEvents();  
+   };
+
+const projectFormEvents = () => {
+    const projectFormElement = document.querySelector('#projectForm')
+    projectFormElement.addEventListener("submit", handleProjectForm);
+};
+
 
 const init = () => {
+    projectForm();
     projectResult(projectArr);
 }
 
